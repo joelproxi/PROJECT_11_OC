f057cf88c57edfa3aa857e31d067a0768b3644f8
Evan-Snd <78089927+Evan-Snd@users.noreply.github.com>
2024-03-26 15:47:45 +0100

    Update server.py

--- server.py
+++ server.py
@@ -61,6 +61,7 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+    placesAvailable = int(competition['numberOfPlaces'])
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
