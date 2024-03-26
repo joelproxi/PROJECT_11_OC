825ef768eea9dc82a73523287df6bd6a39a47057
Evan-Snd <78089927+Evan-Snd@users.noreply.github.com>
2024-03-26 15:33:42 +0100

    Update server.py

--- server.py
+++ server.py
@@ -24,11 +24,6 @@ clubs = loadClubs()
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
-def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
-
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
